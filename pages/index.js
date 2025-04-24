9e7a5732061e118123191d4961dea638cde2d584
jangna93 <47217098+jangna93@users.noreply.github.com>
2025-04-24 22:11:55 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
         <title>NEOSEOUL - Fashion Inspired By Seoul</title>
         <meta name="description" content="New Soul, New You - Global Fashion Select Shop" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <HeroSection />
