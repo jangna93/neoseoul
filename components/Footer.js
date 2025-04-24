bdfc5cbaf4256f7de771f6a40303ccbbbbb3b40f
jangna93 <47217098+jangna93@users.noreply.github.com>
2025-04-24 21:50:55 +0900

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>상호: NEOSEOUL</p>
-      <p>사업자등록번호: 557-19-02656</p>
+      <p>Business Name: NEOSEOUL</p>
+      <p>Business Registration Number: 557-19-02656</p>
       <p>© 2025 NEOSEOUL. All rights reserved.</p>
     </footer>
   );
